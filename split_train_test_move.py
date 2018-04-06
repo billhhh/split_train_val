fcb8706f6ef4ebd895afa745f175b85883a87487
Bill Wang <billhhh@users.noreply.github.com>
2018-04-06 14:16:00 +0800

    split_train_test_move.py name united

--- split_train_test_move.py
+++ split_train_test_move.py
@@ -6,9 +6,9 @@ from random import shuffle
 import shutil
 
 list_path = './'
-train_list = 'train.txt'
-val_list = 'test.txt'
-# val_ratio = 0.8
+train_list = 'train_val.txt'
+test_list = 'test.txt'
+# test_ratio = 0.8
 
 src_dir = 'train_val'
 des_dir = 'test'
@@ -30,7 +30,7 @@ with open(os.path.join(list_path,'food_id.txt'), 'w') as f:
         f.writelines(str(ind) + ":" + lst[ind] + '\n')
 
 f_train = open(os.path.join(list_path, train_list), 'w')
-f_val = open(os.path.join(list_path, val_list), 'w')
+f_test = open(os.path.join(list_path, test_list), 'w')
 
 for ind in range(0,len(lst)):
     os.mkdir(des_dir + '/' + lst[ind])
@@ -46,11 +46,11 @@ for ind in range(0,len(lst)):
             cnt+=1
             # fix test set(50 images)
             if cnt <= 50:
-                f_val.writelines(filepath_src + " " + str(ind) + '\n')
+                f_test.writelines(filepath_src + " " + str(ind) + '\n')
                 # move files
                 shutil.move(filepath_src, des_dir+ '/' + lst[ind] + '/' + pic_name)
             else:
                 f_train.writelines(filepath_src + " " + str(ind) + '\n')
 
 f_train.close()
-f_val.close()
+f_test.close()
